a30b292ed1270fe8325c017a4762483dd53376d7
Bram Reyniers <reyniersbram@gmail.com>
2025-10-14 23:13:40 +0200

    add assembly arm version

---
+++ assembly/arm/hello.s
@@ -0,0 +1,14 @@
+.syntax unified
+.global main
+main:
+    push    {ip, lr}
+
+    ldr     r0, =message
+    bl      printf
+
+    mov     r0, #0
+
+    pop     {ip, pc}
+
+message:
+    .asciz "Hello, world.\n"
